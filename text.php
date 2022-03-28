5d47c661373033ee5cceb2bfe25c21d7fa5e3889
Arjun7161 <akarjun7161@gmail.com>
2022-03-28 16:29:04 +0100

    texts

--- text.php
+++ text.php
@@ -29,7 +29,7 @@ if(!isset($_SESSION['username'])){
             <ul>
             <h1>Welcome <?php echo $_SESSION['username']; ?> </h1>
                 <li><a class="nav-link" href="index.php">Home</a></li>
-                <li><a class="nav-link" href="about.php">About Us</a></li>
+                <li><a class="nav-link" href="storylist.php">Stories</a></li>
                 <li><a class="nav-link" href="upload.php">Upload Image</a></li>
                 <li><a class="nav-link" href="location.php">Location</a></li>
                 <li><a class="nav-link" href="logout.php">Log Out</a></li>
