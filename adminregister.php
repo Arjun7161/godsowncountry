da9e4180021958b0fb1500dbf9f16300b86307c2
Arjun7161 <akarjun7161@gmail.com>
2022-03-29 11:19:59 +0100

    asd

--- adminregister.php
+++ adminregister.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-header('location:main.php');
+header('location:admin.php');
 $con= mysqli_connect('localhost','root','');
 $errors=array();
 mysqli_select_db($con,'cmm007');
